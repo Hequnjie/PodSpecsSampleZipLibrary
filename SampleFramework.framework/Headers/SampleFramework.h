c262cf581713059f0d4e49b19beba41f152109c6
Hequnjie <360606411@qq.com>
2016-07-19 14:50:34 +0800

    添加SampleFramework V0.0.1  build 100001

---
+++ SampleFramework.framework/Headers/SampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by Hequnjie on 16/7/19.
+//  Copyright © 2016年 Nationsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
